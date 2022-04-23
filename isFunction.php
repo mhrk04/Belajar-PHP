7ecda4411568b4de4117b2d5e783582ea6b7b2a2
mhrk04 <haziqrohaizan@gmail.com>
2022-04-23 09:11:11 +0800

    functions

---
+++ isFunction.php
@@ -0,0 +1,5 @@
+<?php
+$data = 12345;
+$data2 = [1, 2, 3, 4, 5, 6, 7];
+var_dump(is_int($data));
+var_dump(is_array($data2));
---
+++ stringFunction.php
@@ -0,0 +1,10 @@
+<?php
+var_dump(join(",", [10, 11, 12, 13, 14, 15]));
+
+var_dump(
+  explode(" ", "Haziq ROhaizan")
+);
+
+var_dump(strtolower("MHAZIQ"));
+var_dump(strtoupper("haziq"));
+var_dump(substr("haziqrk", 0, 7));
